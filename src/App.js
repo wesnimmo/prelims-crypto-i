38dfe9073c0f36351d5582e7350d682ceb363112
Wes <wesnimmo@gmail.com>
2024-01-16 21:52:31 -0800

    prelims/crypto-i

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Featured from './components/Featured'
+import Footer from './components/Footer'
+import Hero from './components/Hero'
+import Navbar from './components/Navbar'
+import Signup from './components/Signup'
+
+function App() {
+  return (
+    <>
+     <Navbar />
+     <Hero />
+     <Featured />
+     <Signup />
+     <Footer />
+    </>
+  );
+}
+
+export default App;
